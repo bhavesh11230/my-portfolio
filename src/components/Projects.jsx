cb0c8ba09ddb11aca7fdf43f1c095a47986344e6
bhavesh11230 <bhaveshrajput048@gmail.com>
2025-07-18 16:35:19 +0530

    updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = () => {
     {
       title: "Fuel Indeed",
       description: "Fuel booking system with real-time availability and admin control. Users can book fuel, view history. Admins can manage bookings and view analytics.",
-      image: "/profileimg/Fuel.jpg",
+      image:"/fuel.jpg",
       technologies: ["Java", "Spring", "JSP", "Servlets", "MySQL", "JavaScript", "HTML", "CSS"],
       category: "Full Stack",
       github: "https://github.com/bhavesh11230/Fuel_Indeed-Fuel_Booking_Web_Application.git",
